47d08133b67eaa886b1a8ddec08a667d2f6458fc
Kodart <arthurguimaraespds@gmail.com>
2025-09-11 11:55:25 -0300

    atividades 11/09

---
+++ src/Principal.java
@@ -0,0 +1,18 @@
+import com.google.www.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println(meuFilme.getTotalDeAvaliacoes());
+        System.out.println(meuFilme.pegaMedia());
+
+    }
+}
